2880f6dac646f42f6ba4298f7da4147d310a315a
jyotendra02 <jyotendrachandan02@gmail.com>
2024-09-08 23:06:39 +0530

    test2

--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -1,5 +1,7 @@
-{
-  "compilerOptions": {
-    "isolatedModules": false
-  }
-}// TODO 8 - Fetch storage of the Lottery by completing fetchStorage
+import React from 'react'
+
+export const tzkt = () => {
+  return (
+    <div></div>
+  )
+}
